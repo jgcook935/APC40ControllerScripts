ecd2eea325dfdba85669423723dc72bbb4f9cf7b
James Cook <james@trainerroad.com>
2019-12-27 10:36:25 -0800

    add simple panning support

--- TrackHandler.js
+++ TrackHandler.js
@@ -1,8 +1,24 @@
+load("Helpers.js");
+
+// for bank select buttons, these will move the trackbank forward and back
 const SELECT_RIGHT = 0x60;
 const SELECT_LEFT = 0x61;
+
+// for the block of activator/solo/cue/arm buttons
 const MUTE = 0x32;
 const SOLO = 0x31;
 const ARM = 0x30;
+
+// for the track control knobs
+// TODO: add variables to track whether the user has selected pan/sendA/sendB/sendC
+const KNOB_1 = 0x30;
+const KNOB_2 = 0x31;
+const KNOB_3 = 0x32;
+const KNOB_4 = 0x33;
+const KNOB_5 = 0x34;
+const KNOB_6 = 0x35;
+const KNOB_7 = 0x36;
+const KNOB_8 = 0x37;
 
 TrackHandler = (trackbank, cursorTrack) => {
     this.trackbank = trackbank;
@@ -41,77 +57,46 @@ TrackHandler = (trackbank, cursorTrack) => {
 // eventually be able to hook up the scene and clip launching here too
 
 TrackHandler.prototype.handleMidi = function(status, data1, data2) {
-    var channel = status >= 0x90 ? status - 0x90 : status - 0x80;
+    let channel;
+    if (isChannelController(status) && inRange(data1, 0x30, 0x37)) {
+        // TODO: check whether pan, or any of the sends are selected here
+        this.trackbank
+            .getItemAt(data1 - 0x30)
+            .pan()
+            .set(data2, 128);
+    } else {
+        channel = status >= 0x90 ? status - 0x90 : status - 0x80;
 
-    switch (data1) {
-        case MUTE:
-            this.trackbank
-                .getItemAt(channel)
-                .mute()
-                .toggle();
-            return true;
-        case SOLO:
-            this.trackbank
-                .getItemAt(channel)
-                .solo()
-                .toggle();
-            return true;
-        case ARM:
-            this.trackbank
-                .getItemAt(channel)
-                .arm()
-                .toggle();
-            return true;
-        case SELECT_LEFT:
-            this.trackbank.scrollPageBackwards();
-            return true;
-        case SELECT_RIGHT:
-            this.trackbank.scrollPageForwards();
-            return true;
-        default:
-            return false;
+        switch (data1) {
+            case MUTE:
+                this.trackbank
+                    .getItemAt(channel)
+                    .mute()
+                    .toggle();
+                return true;
+            case SOLO:
+                this.trackbank
+                    .getItemAt(channel)
+                    .solo()
+                    .toggle();
+                return true;
+            case ARM:
+                this.trackbank
+                    .getItemAt(channel)
+                    .arm()
+                    .toggle();
+                return true;
+            case SELECT_LEFT:
+                this.trackbank.scrollPageBackwards();
+                return true;
+            case SELECT_RIGHT:
+                this.trackbank.scrollPageForwards();
+                return true;
+            default:
+                return false;
+        }
     }
 };
-
-// TrackHandler.prototype.handleMidi = function(status, data1, data2) {
-//   if (isNoteOn(status)) {
-//     switch (data1) {
-//       case MOXF_BUTTON_SF1:
-//         this.trackbank.getItemAt(0).select();
-//         return true;
-
-//       case MOXF_BUTTON_SF2:
-//         this.trackbank.getItemAt(1).select();
-//         return true;
-
-//       case MOXF_BUTTON_SF3:
-//         this.trackbank.getItemAt(2).select();
-//         return true;
-
-//       case MOXF_BUTTON_SF4:
-//         this.trackbank.getItemAt(3).select();
-//         return true;
-
-//       case MOXF_BUTTON_SF5:
-//         this.trackbank.scrollPageBackwards();
-//         return true;
-
-//       case MOXF_BUTTON_SF6:
-//         this.trackbank.scrollPageForwards();
-//         return true;
-
-//       case MOXF_BUTTON_SOLO:
-//         this.cursorTrack.solo().toggle();
-//         return true;
-
-//       case MOXF_BUTTON_MUTE:
-//         this.cursorTrack.mute().toggle();
-//         return true;
-
-//       default:
-//         return false;
-//     }
-//   }
 
 //   if (isChannelController(status)) {
 //     switch (data1) {
