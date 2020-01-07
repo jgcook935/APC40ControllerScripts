2398c798c1c812f1f5589f62e3a3d1ab5306c996
James Cook <james@trainerroad.com>
2020-01-06 21:40:59 -0800

    add some update led methods

--- APC40Hardware.js
+++ APC40Hardware.js
@@ -7,9 +7,18 @@ Apc40Hardware = (outputPort, inputPort, inputCallback) => {
     this.portIn.setMidiCallback(inputCallback);
 };
 
-Apc40Hardware.prototype.updateLED = (note, isOn) => {
-    var value = isOn ? 127 : 0;
-    if (this.ledCache[note] == value) return;
-    this.ledCache[note] = value;
-    this.portOut.sendMidi(0x90, note, value);
+Apc40Hardware.prototype.updateLed = (isOn, id) => {
+    this.portOut.sendMidi(isOn ? 0x90 : 0x80, id, isOn ? 1 : 0);
+};
+
+Apc40Hardware.prototype.updateChannelLed = (isOn, channel, id) => {
+    this.portOut.sendMidi((isOn ? 0x90 : 0x80) + channel, id, isOn ? 1 : 0);
+};
+
+Apc40Hardware.prototype.updateDeviceKnobLed = (id, data) => {
+    this.portOut.sendMidi(0xb0, id, data);
+};
+
+Apc40Hardware.prototype.sendMode = mode => {
+    this.portOut.sendSysex([0xf0, 0x47, 0x00, 0x73, 0x60, 0x00, 0x04, mode, 0x08, 0x04, 0x01, 0xf7]);
 };
