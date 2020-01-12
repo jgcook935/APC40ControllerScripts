248ef550516d960489282dd0f239a12cddf0d926
James Cook <james@trainerroad.com>
2020-01-11 18:36:12 -0800

    remove master track handler

--- APC40.control.js
+++ APC40.control.js
@@ -23,10 +23,10 @@ let transport;
 init = () => {
     hardware = new Apc40Hardware(host.getMidiOutPort(0), host.getMidiInPort(0), handleMidi);
     app = new ApplicationHandler(host.createApplication());
-    master = new MasterTrackHandler(host.createMasterTrack(8));
+    master = host.createMasterTrack(8);
 
     const cursorTrack = host.createCursorTrack("APC40_CURSOR_TRACK", "Cursor Track", 3, 5, true);
-    track = new TrackHandler(host.createMainTrackBank(8, 3, 5), cursorTrack, hardware);
+    track = new TrackHandler(host.createMainTrackBank(8, 3, 5), cursorTrack, hardware, master);
 
     const cursorDevice = cursorTrack.createCursorDevice(
         "APC40_CURSOR_DEVICE",
@@ -50,7 +50,6 @@ handleMidi = (status, data1, data2) => {
     if (device.handleMidi(status, data1, data2)) return;
     if (transport.handleMidi(status, data1, data2)) return;
     if (app.handleMidi(status, data1, data2)) return;
-    if (master.handleMidi(status, data1, data2)) return;
 };
 
 flush = () => {};
